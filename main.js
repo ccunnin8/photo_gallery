7f5d6211c8b0fbcc438169b82d9c91ffd96cfdcc
Corey Cunningham <coreyjjc@me.com>
2016-03-12 16:48:01 -0500

    fixed the issues that were brought up upon submission, works fine now with chrome, search works backwards now too

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ var $searchBox = $("input");
 var $images = imageObjects;
 var $container = $(".container");
 var $image = $("<img class='small-img'>")
-showAllBoxes();
+
+//runs through the javascript object with all the photos and displays them on the page when the page loads 
 function showAllBoxes() {
 	$.each($images,function(index,value){
 		var url = "Photos/Thumbnails/" + this.name;
 		var $new_image = $("<div class='thumbnail'><img class='small-img " + this.title + "' src=" + url + "></div>");
 		$container.append($new_image);
 	});
+	$(".small-img").on("click",clickOnThumbnail);
 }
+
 function searchEmpty(){
 	return ($searchBox.val() === "");
 }
@@ -23,48 +26,48 @@ function imageSearch(expression,string) {
 function removeNotMatchImages() {
 	var textInSearchBox = $searchBox.val();
 	var tester = new RegExp(textInSearchBox,"g");
-	for (var i = 0; i < $images.length; i++ ){ 
+	for (var i = 0; i < $images.length; i++ ){
+		var $currentImage = $("." + $images[i].title);
 		//if search value = alt/caption of image 
-	if ((!imageSearch(tester,$images[i].caption) && !imageSearch(tester,$images[i].title)) && !searchEmpty() ) {
-		$("." + $images[i].title).parent().remove();
+	if (!imageSearch(tester,$images[i].caption) && !imageSearch(tester,$images[i].title) && !searchEmpty() ) {
+		$currentImage.parent().removeClass("thumbnail").addClass("hidden");
 		}
-	}
+	else {
+		$currentImage.parent().addClass("thumbnail").removeClass("hidden");
+		}
+	} 
 }
 //when the search bar is completely empty, removes all current boxes and adds back all the original photos
-function reverseToOriginal(){
-	if (searchEmpty()){
-		$(".thumbnail").remove();
-		showAllBoxes();
-	}
-}
+showAllBoxes();
 
 $searchBox.keyup(function(){
 	removeNotMatchImages();
-	reverseToOriginal();
 	$(".small-img").on("click",clickOnThumbnail);
 })
 
-$(".small-img").on("click",clickOnThumbnail);
+
 //PHOTOVIEWER 
 ///******////////////////////////
-var $photoBox = $("<div class='large-img'></div>");
+var $photoviewer = $("<div class='photoviewer'></div>");
+var $photoBox = $("<img class='large-img'></img>");
 var $leftClick = $("<div class='left-click'><</div>");
 var $rightClick = $("<div class='right-click'>></div>");
-$caption = $("<p class='caption'></p>");
+var $caption = $("<p class='caption'></p>");
 var total_num_slides = 12;
-
+//gets src of image from thumbnail, converts it to Large img src and changes 
+//photobox attribute to the large img src 
 function getPhotoUrl(thumbnail) {
 	var photo = thumbnail.attr("src");
 	photo = photo.slice(18);
 	caption(parseInt(photo)-1);
 	var bigger_photo = "Photos/" + photo;
-	return bigger_photo;
+	$photoBox.attr("src",bigger_photo);
 }
-function getBackgroundImage(div) {
-	return div.css("background-image");
+function getBackgroundImage(img) {
+	return img.attr("src");
 }
 function slideNumber(slide){
-	return parseInt(slide.slice(-7));
+	return parseInt(slide.slice(-6));
 }
 function nextImageUrl(number) {
 	var oldUrl = getBackgroundImage($photoBox);
@@ -75,17 +78,15 @@ function nextImageUrl(number) {
 		var insertNum = number.toString();
 	}
 	caption(number-1);
-	return oldUrl.slice(0,-7) + insertNum + oldUrl.slice(-5); 
+	return 'Photos/' + insertNum + '.jpg'; 
 }
 //When user clicks on thumbnail large image shows
 
 function clickOnThumbnail() {
-	$photoBox.css("background","url(" + getPhotoUrl($(this)) + ") no-repeat");
-	$photoBox.css("background-size","contain");
-	$container.append($photoBox);
+	getPhotoUrl($(this));
+	$container.append($photoviewer.append($photoBox).append($caption));
 	$photoBox.after($leftClick);
 	$photoBox.before($rightClick);
-	$(".container").append($caption);
 	$("html").css("background","gray");
 	//All other images are not visible
 	$(".thumbnail").addClass("greyed-out");
@@ -94,20 +95,20 @@ function clickOnThumbnail() {
 	$rightClick.on("click",rightClick);
 }
 
-$(".thumbnail").on("click",clickOnThumbnail);
+// $(".thumbnail").on("click",clickOnThumbnail);
 //When user clicks on thumbnail
 $(document).mouseup(function(e) {
 	if (!$photoBox.is(e.target) && $photoBox.has(e.target).length === 0 && !$leftClick.is(e.target) && !$rightClick.is(e.target)) {
-		$photoBox.remove();
-		$leftClick.remove();
-		$rightClick.remove();
+		$(".photoviewer").remove();
 		$caption.remove();
 		$("html").css("background","white");
 		$("img").removeClass("greyed-out");
 		$('.small-img').show();
 	}
 });
-
+function changeImage(slideNum){
+	$photoBox.attr("src",nextImageUrl(slideNum));
+}
 //User can scroll to other images using left and right arrows 
 function leftClick() {
 	var slideUrl = getBackgroundImage($photoBox);
@@ -118,7 +119,7 @@ function leftClick() {
 	else {
 		slideNum -= 1;
 	}
-	$photoBox.css("background-image",nextImageUrl(slideNum));
+	changeImage(slideNum);
 }
 
 function rightClick() {
@@ -130,7 +131,7 @@ function rightClick() {
 	else {
 		slideNum += 1;
 	}
-	$photoBox.css("background-image",nextImageUrl(slideNum));
+	changeImage(slideNum);
 }
 function caption(slideNum) {
 	$caption.text($images[slideNum].caption);
